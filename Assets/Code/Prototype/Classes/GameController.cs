2ecf91d8cb977a807fd95cb4f87e8fd7bf6a895d
James Johnson <music1384@yahoo.co.uk>
2017-02-05 20:05:59 +0000

    Created GameController class

---
+++ Assets/Code/Prototype/Classes/GameController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.Prototype.Classes
+{
+    [AddComponentMenu("Controllers/Game Controller")]
+    public class GameController : MonoBehaviour
+    {
+
+    }
+}
