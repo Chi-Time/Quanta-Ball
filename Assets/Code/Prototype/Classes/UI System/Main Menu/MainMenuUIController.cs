aa1128f1e29c8c7c7820373e8f043f42151491e9
James Johnson <music1384@yahoo.co.uk>
2017-02-05 20:59:45 +0000

    Generated meta files for classes.

---
+++ Assets/Code/Prototype/Classes/UI System/Main Menu/MainMenuUIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class MainMenuUIController : MonoBehaviour
+    {
+        public static MainMenuUIController Instance = null;
+
+        public MenuUIController _MenuController = null;
+        public OptionsUIController _OptionsController = null;
+        public CreditsUIController _CreditsController = null;
+
+        private void Awake ()
+        {
+        }
+
+        private void OnUISwitch ()
+        {
+        }
+
+        private void SwitchScreen (GameObject screen)
+        {
+        }
+    }
+}
