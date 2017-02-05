f88a46f4647276ff25501e3e14e7ff826d8fb7b1
James Johnson <music1384@yahoo.co.uk>
2017-02-05 19:42:05 +0000

    Implemented pool logic and serialised fields.

--- Assets/Code/Prototype/Classes/Pool.cs
+++ Assets/Code/Prototype/Classes/Pool.cs
@@ -4,80 +4,114 @@ using System.Collections.Generic;
 
 namespace Assets.Code.Prototype.Classes
 {
-    public class Pool : MonoBehaviour
+    public class Pool
     {
         [Header("Configuration")]
         [Tooltip("The number of floors pre-spawned into the floors pool.")]
-        private int _FloorPoolSize = 35;
+        [SerializeField] private int _FloorPoolSize = 35;
         [Tooltip("The number barriers pre-spawned into the barriers pool.")]
-        private int _BarrierPoolSize = 12;
+        [SerializeField] private int _BarrierPoolSize = 12;
 
         [Space(20)]
         [Header("Objects")]
         [Tooltip("The floor prefab to use.")]
-        private Piece _FloorPrefab = null;
+        [SerializeField] private Piece _FloorPrefab = null;
         [Tooltip("The barrier prefab to use.")]
-        private Piece _BarrierPrefab = null;
+        [SerializeField] private Piece _BarrierPrefab = null;
 
         [Space (20)]
         [Header("Pools")]
         [Tooltip("The object pool of Floors. NOTE: Read only, don't edit.")]
-        private List<Piece> _Floors = new List<Piece> ();
-
+        [SerializeField] private List<Piece> _Floors = new List<Piece> ();
         [Tooltip ("The object pool of Barriers. NOTE: Read only, don't edit.")]
-        private List<Piece> _Barriers = new List<Piece> ();
+        [SerializeField] private List<Piece> _Barriers = new List<Piece> ();
 
-        private MonoBehaviour _Behaviour = null;
-
-        public void Init (MonoBehaviour behaviour)
+        public void Init ()
         {
-            _Behaviour = behaviour;
             GeneratePools ();
         }
 
         private void GeneratePools ()
         {
+            for (int i = 0; i < _FloorPoolSize; i++)
+                _Floors.Add (SpawnFloor ());
 
+            for (int i = 0; i < _BarrierPoolSize; i++)
+                _Barriers.Add (SpawnBarrier ());
         }
 
         private Piece SpawnFloor ()
         {
-            return null;
+            var go = (GameObject)Object.Instantiate (_FloorPrefab.gameObject, Vector3.zero, Quaternion.identity);
+
+            var c = SetupObject (go, "World", "Floor").GetComponent<Piece> ();
+            c.Pool = this;
+
+            return c;
         }
 
         private Piece SpawnBarrier ()
         {
-            return null;
+            var go = (GameObject)Object.Instantiate (_BarrierPrefab.gameObject, Vector3.zero, Quaternion.identity);
+
+            var c = SetupObject (go, "World", "Barrier").GetComponent<Piece> ();
+            c.Pool = this;
+
+            return c;
         }
 
         private GameObject SetupObject (GameObject go, string parent, string name)
         {
-            return null;
+            go.transform.SetParent (GameObject.Find (parent).transform);
+            go.SetActive (false);
+            go.name = name;
+
+            return go;
         }
 
         public Piece RetrieveFloor ()
         {
+            if(_Floors.Count > 0)
+            {
+                var floor = _Floors[0];
+                _Floors.Remove (floor);
+                floor.gameObject.SetActive (true);
+                return floor;
+            }
+
             return null;
         }
 
         public Piece RetrieveBarrier ()
         {
+            if (_Barriers.Count > 0)
+            {
+                var barrier = _Barriers[0];
+                _Barriers.Remove (barrier);
+                barrier.gameObject.SetActive (true);
+                return barrier;
+            }
+
             return null;
         }
 
         public void ReturnFloor (Piece floor)
         {
-
+            _Floors.Add (ResetPiece (floor));
         }
 
         public void ReturnBarrier (Piece barrier)
         {
-
+            _Barriers.Add (ResetPiece (barrier));
         }
 
         private Piece ResetPiece (Piece piece)
         {
-            return null;
+            piece.transform.localScale = Vector3.one;
+            piece.transform.position = Vector3.zero;
+            piece.gameObject.SetActive (false);
+
+            return piece;
         }
     }
 }
