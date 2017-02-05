d03686c2c2f9429e87f03ec6e67413926b0db1bd
James Johnson <music1384@yahoo.co.uk>
2017-02-05 19:08:20 +0000

    Implemented Barrier logic

--- Assets/Code/Prototype/Classes/Barrier.cs
+++ Assets/Code/Prototype/Classes/Barrier.cs
@@ -1,10 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 namespace Assets.Code.Prototype.Classes
 {
     public class Barrier : Piece
     {
+        /// <summary>
+        /// The number of shrinks the object can do before it's culled.
+        /// </summary>
+        private int _ShrinkAmount = 4;
+        /// <summary>
+        /// Cached amount value so that amount can be restored upon object reset.
+        /// </summary>
+        private int _CachedShrinkAmount = 0;
+        /// <summary>
+        /// The scale to shrink the object by every time.
+        /// </summary>
+        private float _ShrinkScale = 0f;
 
+        protected override void AssignValues ()
+        {
+            base.AssignValues ();
+
+            _ShrinkScale = 1 / (float)_ShrinkAmount;
+            _CachedShrinkAmount = _ShrinkAmount;
+        }
+
+        private void OnEnable ()
+        {
+            _ShrinkAmount = _CachedShrinkAmount;
+
+            Move ();
+        }
+
+        private void OnCollisionEnter (Collision collision)
+        {
+            if (collision.gameObject.CompareTag ("Player"))
+                Shrink ();
+        }
+
+        private void Shrink ()
+        {
+            _Transform.localScale = new Vector3 (
+                _Transform.localScale.x - _ShrinkScale,
+                _Transform.localScale.y - _ShrinkScale,
+                _Transform.localScale.z - _ShrinkScale
+                );
+
+            _ShrinkAmount--;
+
+            if (_ShrinkAmount <= 0)
+                Cull ();
+        }
+
+        public override void Cull ()
+        {
+            //TODO: Return item to pool.
+        }
     }
 }
