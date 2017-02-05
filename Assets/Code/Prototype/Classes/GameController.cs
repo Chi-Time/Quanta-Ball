07864c91a304e454a8512427ca07ee8ff6cbccd8
James Johnson <music1384@yahoo.co.uk>
2017-02-05 20:08:28 +0000

    Implemented scoring system in gamecontroller

--- Assets/Code/Prototype/Classes/GameController.cs
+++ Assets/Code/Prototype/Classes/GameController.cs
@@ -6,6 +6,25 @@ namespace Assets.Code.Prototype.Classes
     [AddComponentMenu("Controllers/Game Controller")]
     public class GameController : MonoBehaviour
     {
+        public int Score { get { return _Score; } set { ChangeScore (value); } }
 
+        public static GameController Instance = null;
+
+        [SerializeField] private int _Score = 0;
+
+        private void Awake ()
+        {
+            Instance = this;
+        }
+
+        private void ChangeScore (int score)
+        {
+            _Score = score;
+
+            if(score % 5 == 0)
+            {
+                //TODO: Change player's movement speed.
+            }
+        }
     }
 }
