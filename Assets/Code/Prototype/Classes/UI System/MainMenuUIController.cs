d93cd38a90c725bbb1b0afc51d5d064485ecf458
James Johnson <music1384@yahoo.co.uk>
2017-02-05 20:54:42 +0000

    Created blank classes ready for main menu ui.

---
+++ Assets/Code/Prototype/Classes/UI System/MainMenuUIController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class MainMenuUIController : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Code/Prototype/Classes/UI System/OptionsUIController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class OptionsUIController : MonoBehaviour
+    {
+
+    }
+}
+
