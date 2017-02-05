b96c325767123ae43be0706d4e4843f63c62fd90
James Johnson <music1384@yahoo.co.uk>
2017-02-05 20:57:22 +0000

    Templated class ready for logic to be implemented.

--- Assets/Code/Prototype/Classes/UI System/MainMenuUIController.cs
+++ Assets/Code/Prototype/Classes/UI System/MainMenuUIController.cs
@@ -5,6 +5,22 @@ namespace Assets.Code.Prototype.Classes
 {
     public class MainMenuUIController : MonoBehaviour
     {
+        public static MainMenuUIController Instance = null;
 
+        public MenuUIController _MenuController = null;
+        public OptionsUIController _OptionsController = null;
+        public CreditsUIController _CreditsController = null;
+
+        private void Awake ()
+        {
+        }
+
+        private void OnUISwitch ()
+        {
+        }
+
+        private void SwitchScreen (GameObject screen)
+        {
+        }
     }
 }
