94e5ecc0803f3aa3d20144803a1ab7afac313a43
James Johnson <music1384@yahoo.co.uk>
2017-02-05 21:20:40 +0000

    Templated class ready for logic.

--- Assets/Code/Prototype/Classes/UI System/In-Game/InGameUIController.cs
+++ Assets/Code/Prototype/Classes/UI System/In-Game/InGameUIController.cs
@@ -5,6 +5,22 @@ namespace Assets.Code.Prototype.Classes
 {
     public class InGameUIController : MonoBehaviour
     {
+        public static InGameUIController Instance = null;
 
+        public GameUIController _GameScreen = null;
+        public PauseUIController _PauseScreen = null;
+        public GameOverUIController _GameOverScreen = null;
+
+        private void Awake ()
+        {
+        }
+
+        private void OnUISwitch ()
+        {
+        }
+
+        private void SwitchScreen (GameObject screen)
+        {
+        }
     }
 }
