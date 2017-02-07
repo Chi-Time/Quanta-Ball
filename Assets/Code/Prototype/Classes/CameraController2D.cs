0495c66f941490d40b6e257dc0cfd52d1b1afea4
James Johnson <music1384@yahoo.co.uk>
2017-02-07 21:48:16 +0000

    Added 2D camera controller

---
+++ Assets/Code/Prototype/Classes/CameraController2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class CameraController2D : MonoBehaviour
+    {
+        public float DampTime = 0.15f;
+        public Transform Target = null;
+        public bool FindPlayer = true;
+
+        private Vector3 Velocity = Vector3.zero;
+
+        private void Start ()
+        {
+            if(FindPlayer)
+            {
+                Target = GameObject.FindGameObjectWithTag ("Player").transform;
+            }
+        }
+
+        void FixedUpdate ()
+        {
+            if (Target)
+            {
+                Vector3 point = Camera.main.WorldToViewportPoint (Target.position);
+
+                Vector3 delta = Target.position - Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+                Vector3 destination = transform.position + delta;
+
+                transform.position = Vector3.SmoothDamp (transform.position, destination, ref Velocity, DampTime);
+            }
+
+        }
+    }
+}
