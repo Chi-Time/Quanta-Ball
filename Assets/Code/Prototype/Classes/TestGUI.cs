40cb7411efd35e8239143d7b71f7c86b32fb330b
James Johnson <music1384@yahoo.co.uk>
2017-02-08 19:13:29 +0000

    Added a test gui for score and retry

---
+++ Assets/Code/Prototype/Classes/TestGUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class TestGUI : MonoBehaviour
+    {
+        private void OnGUI ()
+        {
+            GUI.Label (new Rect (150, 150, 250, 100), "Score: " + GameController.Instance.Score);
+
+            print (GameController.Instance.IsGameOver);
+
+            if (GameController.Instance.IsGameOver)
+            {
+                if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 25, 150, 50), "Restart"))
+                {
+                    SceneManager.LoadScene (0);
+                }
+            }
+        }
+    }
+}
