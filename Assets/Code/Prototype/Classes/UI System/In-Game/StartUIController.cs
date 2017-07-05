0a40632de9f54d68b3568efdc1041cfe39b3ce9b
James Johnson <music1384@yahoo.co.uk>
2017-07-05 14:08:14 +0100

    Added start controller class

---
+++ Assets/Code/Prototype/Classes/UI System/In-Game/StartUIController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class StartUIController : MonoBehaviour
+    {
+        public void StartGame ()
+        {
+
+        }
+
+        public void Exit ()
+        {
+            Application.Quit ();
+        } 
+    }
+}
