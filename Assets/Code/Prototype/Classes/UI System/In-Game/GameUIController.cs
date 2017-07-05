6e0db347334b977cf9feb5914d202921abcdeb37
James Johnson <music1384@yahoo.co.uk>
2017-07-05 14:08:03 +0100

    Changed name of class/variable

--- Assets/Code/Prototype/Classes/UI System/In-Game/GameUIController.cs
+++ Assets/Code/Prototype/Classes/UI System/In-Game/GameUIController.cs
@@ -5,10 +5,53 @@ namespace Assets.Code.Prototype.Classes
 {
     public class GameUIController : MonoBehaviour
     {
+        public enum UIStates { Start, Game, Pause, GameOver };
+        public UIStates CurrentUIState;
+
+        public StartUIController _StartScreen = null;
         public InGameUIController _GameScreen = null;
-        public OptionsUIController _OptionsScreen = null;
+        public PauseUIController _PauseScreen = null;
         public GameOverUIController _GameOverScreen = null;
 
+        void Awake ()
+        {
+            Setup ();
+        }
 
+        void Setup ()
+        {
+            UpdateState (UIStates.Start);
+        }
+
+        public void UpdateState (UIStates stateToActivate)
+        {
+            CurrentUIState = stateToActivate;
+
+            switch (stateToActivate)
+            {
+                case UIStates.Start:
+                    DisplayScreen (_StartScreen.gameObject);
+                    break;
+                case UIStates.Game:
+                    DisplayScreen (_GameScreen.gameObject);
+                    break;
+                case UIStates.Pause:
+                    DisplayScreen (_PauseScreen.gameObject);
+                    break;
+                case UIStates.GameOver:
+                    DisplayScreen (_GameOverScreen.gameObject);
+                    break;
+            }
+        }
+
+        void DisplayScreen (GameObject screenToDisplay)
+        {
+            _StartScreen.gameObject.SetActive (false);
+            _GameScreen.gameObject.SetActive (false);
+            _PauseScreen.gameObject.SetActive (false);
+            _GameOverScreen.gameObject.SetActive (false);
+
+            screenToDisplay.SetActive (true);
+        }
     }
 }
