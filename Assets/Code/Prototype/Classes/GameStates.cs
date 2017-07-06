053cbd93205eb9c2e2921b15153a9a21720f9eda
James Johnson <music1384@yahoo.co.uk>
2017-07-06 02:07:31 +0100

    Added gamestates enum for state control.

---
+++ Assets/Code/Prototype/Classes/GameStates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public enum GameStates
+    {
+        Start,
+        Game,
+        Pause,
+        GameOver,
+        Leaderboards,
+        Stats
+    }
+}
