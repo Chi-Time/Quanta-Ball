441880f4044465c533e669a56df15d0e7aaed711
James Johnson <music1384@yahoo.co.uk>
2017-07-06 14:33:38 +0100

    Implemented basic gameover buttons

--- Assets/Code/Prototype/Classes/UI System/In-Game/GameOverUIController.cs
+++ Assets/Code/Prototype/Classes/UI System/In-Game/GameOverUIController.cs
@@ -5,6 +5,19 @@ namespace Assets.Code.Prototype.Classes
 {
     public class GameOverUIController : MonoBehaviour
     {
+        public void RestartGame ()
+        {
+            EventManager.ChangeState (GameStates.Game);
+        }
 
+        public void Menu ()
+        {
+            EventManager.ChangeState (GameStates.Start);
+        }
+
+        public void Quit ()
+        {
+            EventManager.ChangeState (GameStates.Pause);
+        } 
     }
 }
