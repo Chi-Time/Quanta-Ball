16d75ee4515c8bb816972eb7dfcc0c65f4b02a45
James Johnson <music1384@yahoo.co.uk>
2017-07-06 02:07:45 +0100

    Changed name of Eventmanger and implemented

---
+++ Assets/Code/Prototype/Classes/EventManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public delegate void SwitchState (GameStates state);
+
+    public static class EventManager
+    {
+        public static event SwitchState OnStateSwitched;
+
+        public static void ChangeState (GameStates state)
+        {
+            OnStateSwitched (state);
+        }
+    }
+}
