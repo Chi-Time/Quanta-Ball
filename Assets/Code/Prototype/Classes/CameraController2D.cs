85eb737be72916832838b03e839e4755f29a1d0a
James Johnson <music1384@yahoo.co.uk>
2017-07-06 05:29:15 +0100

    Refactored class and moved game over check

--- Assets/Code/Prototype/Classes/CameraController2D.cs
+++ Assets/Code/Prototype/Classes/CameraController2D.cs
@@ -13,23 +13,34 @@ namespace Assets.Code.Prototype.Classes
         private void Start ()
         {
             if(FindPlayer)
-            {
                 Target = GameObject.FindGameObjectWithTag ("Player").transform;
-            }
         }
 
         void FixedUpdate ()
         {
+            SetTarget ();
+            MoveCamera ();
+        }
+
+        private void SetTarget ()
+        {
+            if(GameController.Instance.IsGameOver)
+                Target = null;
+            else if (GameController.Instance.IsGameOver != null && Target == null)
+                Target = GameObject.FindGameObjectWithTag ("Player").transform;
+        }
+
+        private void MoveCamera ()
+        {
             if (Target)
             {
-                Vector3 point = Camera.main.WorldToViewportPoint (Target.position);
+                var point = Camera.main.WorldToViewportPoint (Target.position);
 
-                Vector3 delta = Target.position - Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
-                Vector3 destination = transform.position + delta;
+                var delta = Target.position - Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
+                var destination = transform.position + delta;
 
                 transform.position = Vector3.SmoothDamp (transform.position, destination, ref _Velocity, DampTime);
             }
-
         }
     }
 }
