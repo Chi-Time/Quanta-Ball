a6192f5c26901a466b962c3e8c4148054239df26
James Johnson <music1384@yahoo.co.uk>
2017-07-06 04:56:58 +0100

    Created new screen classes

---
+++ Assets/Code/Prototype/Classes/UI System/In-Game/SettingsUIController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class SettingsUIController : MonoBehaviour
+    {
+        //TOOD: Options settings.
+    }
+}
---
+++ Assets/Code/Prototype/Classes/UI System/In-Game/StatsUIController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Code.Prototype.Classes
+{
+    public class StatsUIController : MonoBehaviour
+    {
+        public void ReturnToMenu ()
+        {
+            EventManager.ChangeState (GameStates.Start);
+        }
+    }
+}
