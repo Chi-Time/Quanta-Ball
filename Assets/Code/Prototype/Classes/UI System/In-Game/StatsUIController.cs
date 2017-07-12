301a2796c9224d5c6a41898e2ba766ab71a61a35
James Johnson <music1384@yahoo.co.uk>
2017-07-12 04:33:14 +0100

    Added using and labels.

--- Assets/Code/Prototype/Classes/UI System/In-Game/StatsUIController.cs
+++ Assets/Code/Prototype/Classes/UI System/In-Game/StatsUIController.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
 
 namespace Assets.Code.Prototype.Classes
 {
     public class StatsUIController : MonoBehaviour
     {
+        [SerializeField] private Text MoveCountLabel = null;
+        [SerializeField] private Text TimePlayedLabel = null;
+        [SerializeField] private Text BarrierHitsLabel = null;
+
         public void ReturnToMenu ()
         {
             EventManager.ChangeState (GameStates.Start);
